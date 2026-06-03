ae78fb7383a7c4669b37919bae58e22e5c7e5849
ny-tim-c <tim.cina@noseryoung.ch>
2026-06-03 16:22:46 +0200

    created basic Reservation Hero layout

--- restaurantFrontend/src/elements/pages/reservation.tsx
+++ restaurantFrontend/src/elements/pages/reservation.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import HeroImg from "../../assets/reservationHero.png";
 
 function Reservation() {
   return (
-    <div>
-      <div className="Reservation-Box"></div>
+    <div className="reservation">
+      <div className="Reservation-Hero-Box">
+        <img className="Reservation-Hero" src={HeroImg} alt="" />
+        <div className="Reservation-Hero-shade"></div>
+        <div className="Resevation-Hero-cover"></div>
+        <div className="Reservation-Title">Reservation</div>
+        <div className="Reservation-subtitle">
+          for evryone that hates having no table...
+          <br />
+          <br />
+          this wil be the best solution.
+        </div>
+        <div className="Reservation-Text-Title">How To reserve</div>
+        <div className="Reservation-Text">
+          just fill out the form and enjoy delicious food{" "}
+        </div>
+      </div>
     </div>
   );
 }
