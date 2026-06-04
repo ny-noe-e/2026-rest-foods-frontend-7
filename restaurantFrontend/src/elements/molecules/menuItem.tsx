34c823bdd32cf89b60e6fcdd05827d632e9930aa
ny-tim-c <tim.cina@noseryoung.ch>
2026-06-04 13:24:20 +0200

    added animations to menu

--- restaurantFrontend/src/elements/molecules/menuItem.tsx
+++ restaurantFrontend/src/elements/molecules/menuItem.tsx
@@ -17,23 +17,29 @@ export function Menu_Item({
   return (
     <div
       onClick={() => {
-        setCurrExtended(data.id);
+        if (currExtended === data.id) {
+          setCurrExtended(-1);
+        } else {
+          setCurrExtended(data.id);
+        }
         console.log("pressed");
       }}
     >
       <h1>{data?.name}</h1>
       {data?.chefChoice && <h2>Chef's Choice</h2>}
       <h4>{data?.description}</h4>
-      {currExtended === data.id && (
-        <div>
-          {/* Booleans converted to text so they actually render on screen */}
+
+      <div className="Menu_Details">
+        <div
+          className={`Menu_Details_Animation ${currExtended === data.id ? "is_open" : ""}`}
+        >
           <h5>Gluten Free: {data.glutenFree ? "Yes" : "No"}</h5>
           <h5>Lactose Free: {data.lactoseFree ? "Yes" : "No"}</h5>
           <h5>Vegan: {data.vegan ? "Yes" : "No"}</h5>
           <h5>Vegetarian: {data.vegetarian ? "Yes" : "No"}</h5>
         </div>
-      )}
-      <h5> {data.price} CHF</h5>
+        <h5> {data.price} CHF</h5>
+      </div>
     </div>
   );
 }
--- restaurantFrontend/src/elements/pages/menu.tsx
+++ restaurantFrontend/src/elements/pages/menu.tsx
@@ -1,4 +1,5 @@
 import menuHeroImage from "../../assets/menuHero.png";
+import React from "react";
 import { MenuList } from "../objects/menuList";
 
 function Menu() {
