df88c37c5ca7833fc912f9b11a00b603bff6ab85
ny-tim-c <tim.cina@noseryoung.ch>
2026-06-10 12:12:58 +0200

    added Passowrt check ready to connect to backend

--- restaurantFrontend/src/elements/objects/login.tsx
+++ restaurantFrontend/src/elements/objects/login.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Verify_Password } from "../../tools/VeryfyPassword";
 
 export function Login() {
   const [loginState, setLoginState] = useState<number>();
+  const [password, setPassword] = useState<string>("");
   const handlePassword = (): any => {
-    //make bakend send a password for verification
-    setLoginState(1);
+    if (Verify_Password(password)) setLoginState(1);
   };
   if (loginState == 1) {
     return null;
@@ -13,7 +14,13 @@ export function Login() {
       <div className="loginBackground">
         <div className="loginBox">
           <h1>login</h1>
-          <input type="password" id="password" className="loginPassword" />
+          <input
+            type="password"
+            id="password"
+            className="loginPassword"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <input
             type="submit"
             id="login"
---
+++ restaurantFrontend/src/tools/VeryfyPassword.ts
@@ -0,0 +1,5 @@
+export function Verify_Password(password: string): boolean {
+  //get password from backend
+  const testPassword = "Michi00";
+  return testPassword == password;
+}
