91be8bc7272e5084ef20efbc452f238b37344a69
ny-tim-c <tim.cina@noseryoung.ch>
2026-06-10 09:29:47 +0200

    added working Login (note login has no password set)

--- restaurantFrontend/src/elements/objects/login.tsx
+++ restaurantFrontend/src/elements/objects/login.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export function Login() {
-  return (
-    <div className="loginBackground">
-      <div className="loginBox">
-        <h1>login</h1>
-        <input type="password" className="loginPassword" />
+  const [loginState, setLoginState] = useState<number>();
+  const handlePassword = (): any => {
+    //make bakend send a password for verification
+    setLoginState(1);
+  };
+  if (loginState == 1) {
+    return null;
+  } else {
+    return (
+      <div className="loginBackground">
+        <div className="loginBox">
+          <h1>login</h1>
+          <input type="password" id="password" className="loginPassword" />
+          <input
+            type="submit"
+            id="login"
+            value="login"
+            className="loginButton"
+            onClick={handlePassword}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
